3e27ff469ad17b45cf55a2d79eea647014637592
Alex Moon <alex.moon@tangentlabs.co.uk>
2013-01-13 14:51:30 +0000

    Nginx and Tornado hooray!

--- tornado_main.py
+++ tornado_main.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python
 
-# Run this with
-# PYTHONPATH=. DJANGO_SETTINGS_MODULE=testsite.settings testsite/tornado_main.py
-# Serves by default at
-# http://localhost:8080/hello-tornado and
-# http://localhost:8080/hello-django
-
 from tornado.options import options, define, parse_command_line
 import django.core.handlers.wsgi
 import tornado.httpserver
@@ -14,14 +8,17 @@ import tornado.web
 import tornado.wsgi
 
 import time
+import os
 
-define('port', type=int, default=80)
+define('port', type=int, default=81)
 
 class HelloHandler(tornado.web.RequestHandler):
   def get(self):
     self.write('Hello from tornado')
 
 def main():
+  os.environ['PYTHONPATH'] = '.'
+  os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
   wsgi_app = tornado.wsgi.WSGIContainer(
     django.core.handlers.wsgi.WSGIHandler())
   tornado_app = tornado.web.Application(
