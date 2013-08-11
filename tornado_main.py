f294781f377b4301736b039b8ac872b1415cb80c
Alex Moon <alex.moon@tangentlabs.co.uk>
2013-08-11 15:13:39 +0100

    This is one way of doing it except this doesn't work at all because we don't want a single Tornado instance tracking all the websockets - this is GAE's responsibility - what we really need is a WSGI wrapper of some kind that delegates to Django for everything other than a channel register

--- tornado_main.py
+++ tornado_main.py
@@ -19,13 +19,13 @@ class PollHandler(tornado.websocket.WebSocketHandler):
 
     def on_message(self, message):
         # simple echo so we know stuff's working
-        # self.write_message(json.dumps({'message': message}))
+        self.write_message(json.dumps({'confirm_message': message}))
 
         # same thing but through RabbitMQ
-        self.application.pc.channel.basic_publish(exchange='', routing_key='plase', body=json.dumps({
-            'action': 'test_echo',
-            'message': message,
-        }))
+        # self.application.pc.channel.basic_publish(exchange='', routing_key='plase', body=json.dumps({
+        #     'action': 'test_echo',
+        #     'message': message,
+        # }))
 
     def on_close(self):
         self.application.pc.remove_event_listener(self)
